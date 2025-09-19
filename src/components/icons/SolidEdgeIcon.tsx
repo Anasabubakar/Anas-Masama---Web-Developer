ca6d0088341e6e276036f16db109f7e85737958f
Anas Abubakar <anasabubakar7000@gmail.com>
2025-09-19 22:52:55 +0000

    Give the solid edge icon some color

--- src/components/icons/SolidEdgeIcon.tsx
+++ src/components/icons/SolidEdgeIcon.tsx
@@ -4,16 +4,15 @@ export const SolidEdgeIcon = (props: React.SVGProps<SVGSVGElement>) => (
         xmlns="http://www.w3.org/2000/svg" 
         viewBox="0 0 24 24" 
         fill="none" 
-        stroke="currentColor" 
-        strokeWidth="2" 
+        strokeWidth="1" 
         strokeLinecap="round" 
         strokeLinejoin="round"
     >
-        <path d="M20.2 7.8l-7.4 4.3-7.4-4.3"/>
-        <path d="M12.8 12.1l7.4 4.3"/>
-        <path d="M12 22V12l-7.4 4.3L12 22z"/>
-        <path d="M12 2v10"/>
-        <path d="M3.8 7.8l7.4-4.3 7.4 4.3"/>
-        <path d="M3.8 16.4l7.4 4.3"/>
+        <path d="M20.2 7.8l-7.4 4.3-7.4-4.3" stroke="#0072C6" fill="#0072C6" />
+        <path d="M12.8 12.1l7.4 4.3" stroke="#0072C6" fill="#0072C6"/>
+        <path d="M12 22V12l-7.4 4.3L12 22z" stroke="#0072C6" fill="#0072C6" />
+        <path d="M12 2v10" stroke="#00A3A3" fill="#00A3A3" />
+        <path d="M3.8 7.8l7.4-4.3 7.4 4.3" stroke="#00A3A3" fill="#00A3A3"/>
+        <path d="M3.8 16.4l7.4 4.3" stroke="#00A3A3" fill="#00A3A3"/>
     </svg>
 );
