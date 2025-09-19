7c3cb23a7fb3953bddb6ae9af751e70ee3aa6342
Anas Abubakar <anasabubakar7000@gmail.com>
2025-09-19 22:10:58 +0000

    Move the contents of the header up abit

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -17,13 +17,7 @@ export function Header() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/50 bg-background/80 backdrop-blur-lg">
-      <div className="container flex h-14 items-center">
-        <div className="mr-auto">
-          <Link href="/" className="group flex items-center gap-2 font-bold font-headline uppercase tracking-wider">
-            <Code className="h-6 w-6 text-primary transition-transform duration-300 group-hover:scale-125" />
-            <span className="">Anas Masama</span>
-          </Link>
-        </div>
+      <div className="container flex h-12 items-center">
         <nav className="hidden items-center gap-6 text-sm font-medium md:flex">
           {navLinks.map((link) => (
             <Link
@@ -35,6 +29,12 @@ export function Header() {
             </Link>
           ))}
         </nav>
+        <div className="ml-auto">
+          <Link href="/" className="group flex items-center gap-2 font-bold font-headline uppercase tracking-wider">
+            <Code className="h-6 w-6 text-primary transition-transform duration-300 group-hover:scale-125" />
+            <span className="">Anas Masama</span>
+          </Link>
+        </div>
         <div className="flex flex-1 items-center justify-end gap-2 md:hidden">
           <Button
             variant="ghost"
